f149f77afa627f36531145727b9d0f4e3ce88fe4
pklaus workstation <philipp.klaus@gmail.com>
2009-11-26 02:00:59 +0100

    file to remember me of the actual task

---
+++ test_crop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: UTF8 -*-
+
+# Author: Philipp Klaus, philipp.klaus AT gmail.com
+
+import Image # needs PIL: sudo aptitude install python-imaging
+
+
+# http://www.pythonware.com/library/pil/handbook/image.htm -> crop
