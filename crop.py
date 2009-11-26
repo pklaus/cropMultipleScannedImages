4484afe2402f5006ce38c2650e36928760b49ae4
pklaus workstation <philipp.klaus@gmail.com>
2009-11-26 02:36:26 +0100

    added comment about the next steps

--- crop.py
+++ crop.py
@@ -7,6 +7,13 @@ import Image # needs PIL: sudo aptitude install python-imaging
 import detection
 
 def cropMultipleScannedImages(image):
+    
+    # two notes here:
+    #    - presumably we need corner detection here, not edge detection!!!
+    #      http://cs223b.stanford.edu/notes/CS223B-L3-Features.ppt found here:
+    #      http://stackoverflow.com/questions/1391212/best-articles-to-start-learning-about-edge-detection-image-recognition/1394450#1394450
+    #    - when we have the corners, we still have to find out, which corners belong together and to rotate the images accordingly!
+    
     coordinates = detection.getPictureCoordinates(image)
     pictures = []
     for i in coordinates:
